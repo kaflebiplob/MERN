40e830f3bcf117bd7c6dd585efed9d31da52db90
Biplob kafle <biplobkafle21@gmail.com>
2024-12-26 22:29:35 +0545

    Login frintend dine

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div>
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-lg">Login</h3>
+          <div className="mt-4 space-y-2">
+            <span>email</span> <br />
+            <input
+              type="email"
+              placeholder="enter your email"
+              className="w-80 px-3 border rounded-md outline-none"
+            />
+          </div>
+          <div className="mt-4 space-y-2">
+            <span>password</span> <br />
+            <input
+              type="password"
+              placeholder="enter your password"
+              className="w-80 px-3 border rounded-md outline-none"
+            />
+          </div>
+          <div className="flex justify-around mt-4">
+            <button className="bg-pink-400 font-white rounded-md px-3 py-3 hover:bh-pink-700 duration-300 ">
+              Login
+            </button>
+            <p>
+              Not registered?
+              <a href="" className="text-blue-500 underline ">
+                Signup
+              </a>
+            </p>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+};
+
+export default Login;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Login from "./Login";
 
 const Navbar = () => {
   const NavItems = (
@@ -144,9 +145,15 @@ const Navbar = () => {
               </label>
             </div>
             <div className="">
-              <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer">
+              <a
+                className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer"
+                onClick={() =>
+                  document.getElementById("my_modal_3").showModal()
+                }
+              >
                 Login
               </a>
+              <Login />
             </div>
           </div>
         </div>
