6d125b593a3423658b3cd0959e59712f441d6afd
Biplob kafle <biplobkafle21@gmail.com>
2024-12-27 13:04:20 +0545

    Backend inprogress

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,28 @@
-import express from "express"
-import dotenv from "dotenv"
-
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import bookroute from "./routes/book.route.js";
+const app = express();
+app.use(cors());
 dotenv.config();
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 4000;
+const URI = process.env.MongoDBURI;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// MongoDB
+try {
+  mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  console.log("connected to mongoose");
+} catch (error) {
+  console.log("error", error);
+}
+
+// Routes
+app.use("/book", bookroute);
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
-})
+  console.log(`Example app listening on port ${PORT}`);
+});
---
+++ backend/routes/book.route.js
@@ -0,0 +1,6 @@
+import express from "express";
+import { getBook } from "../controllers/book.controller.js";
+
+const router = express.Router();
+router.get("/", getBook);
+export default router;
