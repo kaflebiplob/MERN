15db8d0fedeeb4c0846a894b38720d785b1a30fb
Biplob kafle <biplobkafle21@gmail.com>
2024-12-27 08:07:12 +0545

    validation done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,44 +1,65 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
       <dialog id="my_modal_3" className="modal">
         <div className="modal-box">
-          <form method="dialog">
-            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+          <form method="dialog" onSubmit={handleSubmit(onSubmit)}>
+            <Link
+              to={"/"}
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
               ✕
-            </button>
+            </Link>
+
+            <h3 className="font-bold text-lg">Login</h3>
+            <div className="mt-4 space-y-2">
+              <span>email</span> <br />
+              <input
+                type="email"
+                placeholder="enter your email"
+                className="w-80 px-3 border rounded-md outline-none"
+                {...register("email", { required: true })}
+              />{" "}
+              <br />
+              {errors.email && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+            <div className="mt-4 space-y-2">
+              <span>password</span> <br />
+              <input
+                type="password"
+                placeholder="enter your password"
+                className="w-80 px-3 border rounded-md outline-none"
+                {...register("password", { required: true })}
+              />{" "}
+              <br />
+              {errors.password && (
+                <span className="text-red-500">This field is required</span>
+              )}
+            </div>
+            <div className="flex justify-around mt-4">
+              <button className="bg-pink-400 font-white rounded-md px-3 py-3 hover:bh-pink-700 duration-300 ">
+                Login
+              </button>
+              <p>
+                Not registered?
+                <Link to="/signup" className="text-blue-500 underline ">
+                  Signup{" "}
+                </Link>
+              </p>
+            </div>
           </form>
-          <h3 className="font-bold text-lg">Login</h3>
-          <div className="mt-4 space-y-2">
-            <span>email</span> <br />
-            <input
-              type="email"
-              placeholder="enter your email"
-              className="w-80 px-3 border rounded-md outline-none"
-            />
-          </div>
-          <div className="mt-4 space-y-2">
-            <span>password</span> <br />
-            <input
-              type="password"
-              placeholder="enter your password"
-              className="w-80 px-3 border rounded-md outline-none"
-            />
-          </div>
-          <div className="flex justify-around mt-4">
-            <button className="bg-pink-400 font-white rounded-md px-3 py-3 hover:bh-pink-700 duration-300 ">
-              Login
-            </button>
-            <p>
-              Not registered?
-              <Link to="/signup" className="text-blue-500 underline ">
-                Signup{" "}
-              </Link>
-            </p>
-          </div>
         </div>
       </dialog>
     </div>
