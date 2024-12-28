7ce6b1bd094889b7f0f9a89198fdb5db1118feec
Biplob kafle <biplobkafle21@gmail.com>
2024-12-28 09:09:35 +0545

    Completed

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -29,7 +29,7 @@ const Banner = () => {
               <input type="text" className="grow" placeholder="Email" />
             </label>
           </div>
-          <button className="btn btn-secondary mt-6">Secondary</button>
+          <button className="btn btn-secondary mt-6">Get Started</button>
         </div>
         <div className="w-full md:w-1/2 order-1">
           <img src="/banner.jpg" className="w-88 h-88 md:w-92 h-92" alt="" />
